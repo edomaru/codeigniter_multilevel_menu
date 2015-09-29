437644b0beee97e63663d45cb1ad8979ddb381e3
edo masaru <masaruedo@yahoo.com>
2015-09-29 10:06:44 +0700

    Replace the sample database

--- ci_multilevel_menu.sql
+++ ci_multilevel_menu.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 30, 2015 at 03:46 PM
--- Server version: 5.5.41-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.7
+-- Generation Time: Sep 29, 2015 at 11:06 AM
+-- Server version: 5.5.43-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS `menus` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent` int(11) DEFAULT NULL,
   `name` varchar(50) NOT NULL,
+  `icon` varchar(30) NOT NULL,
   `slug` varchar(50) NOT NULL,
   `number` int(11) NOT NULL,
   PRIMARY KEY (`id`),
@@ -40,24 +41,24 @@ CREATE TABLE IF NOT EXISTS `menus` (
 -- Dumping data for table `menus`
 --
 
-INSERT INTO `menus` (`id`, `parent`, `name`, `slug`, `number`) VALUES
-(1, NULL, 'Item 0', 'Item-0', 1),
-(2, NULL, 'Item 1', 'Item-1', 2),
-(3, NULL, 'Item 2', 'Item-2', 3),
-(4, NULL, 'Item 3', 'Item-3', 4),
-(5, NULL, 'Item 4', 'Item-4', 5),
-(6, NULL, 'Item 5', 'Item-5', 6),
-(7, NULL, 'Item 6', 'Item-6', 7),
-(8, 1, 'Item 0.1', 'item-0.1', 1),
-(9, 1, 'Item 0.2', 'item-0-2', 2),
-(10, 8, 'Item 0.1.1', 'item-0-1-1', 1),
-(11, 8, 'Item 0.1.2', 'Item-0-1-2', 2),
-(12, 10, 'Item 0.1.1.1', 'Item-0-1-1-1', 1),
-(13, 10, 'Item 0.1.1.2', 'Item-0-1-1-2', 2),
-(14, 2, 'Item 1.1', 'item-1-1', 1),
-(15, 2, 'Item 1.2', 'item-1-2', 2),
-(16, 3, 'Item 2.2', 'item-2-2', 2),
-(17, 3, 'Item 2.1', 'item-2.1', 1);
+INSERT INTO `menus` (`id`, `parent`, `name`, `icon`, `slug`, `number`) VALUES
+(1, NULL, 'Item 0', 'glyphicon glyphicon-user', 'Item-0', 1),
+(2, NULL, 'Item 1', 'glyphicon glyphicon-remove', 'Item-1', 2),
+(3, NULL, 'Item 2', '', 'Item-2', 3),
+(4, NULL, 'Item 3', '', 'Item-3', 4),
+(5, NULL, 'Item 4', '', 'Item-4', 5),
+(6, NULL, 'Item 5', '', 'Item-5', 6),
+(7, NULL, 'Item 6', '', 'Item-6', 7),
+(8, 1, 'Item 0.1', '', 'item-0.1', 1),
+(9, 1, 'Item 0.2', 'glyphicon glyphicon-search', 'item-0-2', 2),
+(10, 8, 'Item 0.1.1', '', 'item-0-1-1', 1),
+(11, 8, 'Item 0.1.2', '', 'Item-0-1-2', 2),
+(12, 10, 'Item 0.1.1.1', '', 'Item-0-1-1-1', 1),
+(13, 10, 'Item 0.1.1.2', '', 'Item-0-1-1-2', 2),
+(14, 2, 'Item 1.1', '', 'item-1-1', 1),
+(15, 2, 'Item 1.2', '', 'item-1-2', 2),
+(16, 3, 'Item 2.2', '', 'item-2-2', 2),
+(17, 3, 'Item 2.1', '', 'item-2.1', 1);
 
 --
 -- Constraints for dumped tables
